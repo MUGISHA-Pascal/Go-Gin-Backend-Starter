9a839988661e488b51d36a6ff6daa981a2834c44
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-07-03 12:37:46 +0200

    feat : finalise on product API endpoints

--- api/products/controller.go
+++ api/products/controller.go
@@ -103,3 +103,51 @@ func DeleteProduct(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{"error": "product deleted successfully"})
 }
+func UpdateProduct(c *gin.Context) {
+	userId, exists := c.Get("userId")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "login first to continue"})
+		return
+	}
+	productId := c.Param("id")
+	if productId == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "product id not provided"})
+		return
+	}
+	var user database.User
+	if err := database.DB.First(&user, userId).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
+		return
+	}
+	if user.Role == "user" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "you are not authorized to perform this action"})
+		return
+	}
+	var product database.Product
+	if err := database.DB.First(&product, productId).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
+		return
+	}
+	var productUpdateDetails ProductUpdate
+	if err := c.ShouldBindJSON(&productUpdateDetails); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if productUpdateDetails.Description != "" {
+		product.Description = productUpdateDetails.Description
+	}
+	if productUpdateDetails.Name != "" {
+		product.Name = productUpdateDetails.Name
+	}
+	if productUpdateDetails.Price != 0 {
+		product.Price = productUpdateDetails.Price
+	}
+	if productUpdateDetails.StockQty != 0 {
+		product.StockQty = productUpdateDetails.StockQty
+	}
+	if err := database.DB.Save(&product).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while updating product"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "product updated successfully", "product": product})
+}
