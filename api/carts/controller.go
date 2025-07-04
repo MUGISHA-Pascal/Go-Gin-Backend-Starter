4a9278c6e923824c3fea3dd1c119d1fbffa20c82
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-07-04 16:49:57 +0200

    refactor : finalise on cart API endpoints

--- api/carts/controller.go
+++ api/carts/controller.go
@@ -13,7 +13,7 @@ type AddToCart struct {
 	CartId    uint `json:"cartId"`
 	Quantity  int  `json:"quantity"`
 }
-type RemoveItemFromCart struct {
+type RemoveItemFromCartDtls struct {
 	ProductId uint `json:"productId"`
 	Quantity  int  `json:"quantity"`
 }
@@ -99,4 +99,56 @@ func AddItemToCart(c *gin.Context) {
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "item added successfully"})
+}
+func RemoveItemToCart(c *gin.Context) {
+	userId, exists := c.Get("userId")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "login to continue"})
+		return
+	}
+	var user database.User
+	if err := database.DB.First(&user, userId).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
+		return
+	}
+	var cart database.Cart
+	if err := database.DB.First(&cart, user.Cart).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cart not found"})
+		return
+	}
+	var removeItemFromCartDtls RemoveItemFromCartDtls
+	if err := c.BindJSON(&removeItemFromCartDtls); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error while binding the request body"})
+		return
+	}
+	var cartItem database.CartItem
+	if err := database.DB.Where("cart_id = ? AND product_id = ?", cart.ID, removeItemFromCartDtls.ProductId).First(&cartItem).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Cart item not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving cart item"})
+		}
+		return
+	}
+
+	if cartItem.Quantity == 1 || cartItem.Quantity == removeItemFromCartDtls.Quantity {
+		// Delete the cart item
+		if err := database.DB.Delete(&cartItem).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to remove item from cart!"})
+			return
+		}
+	} else if cartItem.Quantity > removeItemFromCartDtls.Quantity {
+		// Reduce the cart item's quantity
+		cartItem.Quantity -= removeItemFromCartDtls.Quantity
+		if err := database.DB.Save(&cartItem).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update cart item's quantity!"})
+			return
+		}
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot remove more items than exist in cart!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Cart item updated successfully!"})
+
 }
