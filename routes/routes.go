8043194a1f7e22e638b99ebe101c982975c5d7d4
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-07-05 23:29:20 +0200

    feat : product routes setup

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,28 @@
 package routes
+
+import (
+	"github.com/MUGISHA-Pascal/Go-Backend-Starter/api/products"
+	"github.com/MUGISHA-Pascal/Go-Backend-Starter/api/users"
+	"github.com/MUGISHA-Pascal/Go-Backend-Starter/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) *gin.Engine{
+	r.POST("/users/login",users.LoginUser)
+	r.POST("/users/register",users.RegisterUser)
+	protected := r.Group("/")
+	protected.Use(middleware.Authentication()){
+		setupProductRoutes(protected)
+	}
+	return r
+}
+func setupProductRoutes(rg *gin.RouterGroup){
+	productRoutes := rg.Group("/products")
+	{
+	productRoutes.POST("/create",products.CreateProduct)
+	productRoutes.GET("/all",products.GetAllProducts)
+	productRoutes.GET("/:id",products.GetOneProduct)
+	productRoutes.DELETE("/delete/:id",products.DeleteProduct)
+	productRoutes.PUT("/update/:id",products.UpdateProduct)
+	}
+}
